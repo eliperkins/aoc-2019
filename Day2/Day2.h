c5366200b044bcf52d6a70f7225770096a73c4da
Eli Perkins <eli.j.perkins@gmail.com>
2019-12-02 12:37:27 -0500

    Add day two solutions
    This one was a doozy to understand!

---
+++ Day2/Day2.h
@@ -0,0 +1,19 @@
+//
+//  Day2.h
+//  Day2
+//
+//  Created by Eli Perkins on 12/2/19.
+//  Copyright © 2019 eliperkins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Day2.
+FOUNDATION_EXPORT double Day2VersionNumber;
+
+//! Project version string for Day2.
+FOUNDATION_EXPORT const unsigned char Day2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Day2/PublicHeader.h>
+
+
