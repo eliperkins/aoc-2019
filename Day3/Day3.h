46684ad52da59dfb9b1e25090ffdbb9b0fb1995c
Eli Perkins <eli.j.perkins@gmail.com>
2019-12-03 10:20:19 -0500

    Add day three solution

---
+++ Day3/Day3.h
@@ -0,0 +1,19 @@
+//
+//  Day3.h
+//  Day3
+//
+//  Created by Eli Perkins on 12/3/19.
+//  Copyright © 2019 eliperkins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Day3.
+FOUNDATION_EXPORT double Day3VersionNumber;
+
+//! Project version string for Day3.
+FOUNDATION_EXPORT const unsigned char Day3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Day3/PublicHeader.h>
+
+
